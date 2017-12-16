940b7312b52f774dce8ea2f2c04e393d9aca3ea1
Stan <nedbailov375426@gmail.com>
2017-12-16 00:27:29 -0500

    Update to hapi v17 (#1)
    * Update to hapi v17
    
    * Add strip-ansi to take off colot from message
    
    * Put strip-ansi to devDependencies

--- index.js
+++ index.js
@@ -4,22 +4,14 @@ const Stripe = require('stripe');
 const joi = require('joi');
 const pkg = require('./package.json');
 
-const register = (server, option, done) => {
-    const { error, value : config } = joi.validate(option, joi.object().required().keys({
+const register = (server, option) => {
+    const config = joi.attempt(option, joi.object().required().keys({
         secretKey : joi.string().required().token().min(25).regex(/^sk_/)
     }));
-    if (error) {
-        done(error);
-        return;
-    }
     server.decorate('server', 'stripe', new Stripe(config.secretKey));
-    done();
 };
 
-register.attributes = {
+module.exports.plugin = {
+    register,
     pkg
-};
-
-module.exports = {
-    register
 };
