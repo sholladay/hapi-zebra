de3bf684f1b1fefab255efa4d3b6478ae92475ed
Seth Holladay <me@seth-holladay.com>
2019-06-05 16:05:27 -0400

    Update dependencies

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const pkg = require('./package.json');
 
 const register = (server, option) => {
     const config = joi.attempt(option, joi.object().required().keys({
-        apiVersion : joi.string().optional().length(10).regex(/^\d{4}-\d{2}-\d{2}$/),
-        secretKey  : joi.string().required().token().min(25).regex(/^sk_/)
+        apiVersion : joi.string().optional().length(10).regex(/^\d{4}-\d{2}-\d{2}$/u),
+        secretKey  : joi.string().required().token().min(25).regex(/^sk_/u)
     }));
     const stripe = new Stripe(config.secretKey);
     if (config.apiVersion) {
